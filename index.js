095dce7ce2f27b0d53b300950125773063ab3f1a
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-06-02 20:25:09 +0200

    finished

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    
     if (request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
-            response.end();
+            response.setHeader("Content-Type", "text/html; charset=utf-8");
+            fs.readFile('index.html', 'utf8', function(err, contents) {
+                response.write(contents);
+                response.end();
+            });       
     } else {
-            response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
-
-            fs.readFile('./blad', (err, data) => {
-                if (err) throw err;
-                console.log(data);
-            });
-
-            response.end();
+           response.setHeader("Content-Type", "image/jpg");
+           fs.readFile('blad.jpg', 'utf8', function(err, contents) {
+                response.write(contents);
+                response.end();
+            });  
     }
 });
 
